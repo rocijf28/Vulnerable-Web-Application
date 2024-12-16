760e804a8345d473685dcf9d2d09d087d3f8ed63
rocijf28 <rocijf28@gmail.com>
2024-12-16 17:29:34 +0100

    Update fileupload1.php

--- FileUpload/fileupload1.php
+++ FileUpload/fileupload1.php
@@ -18,15 +18,21 @@
 </form>
 </div>
 <?php
+if (isset($_POST["submit"])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
 
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES["file"]["name"]);
-	
-    move_uploaded_file($_FILES["file"]["tmp_name"], $target_file);
-    echo "File uploaded /uploads/".$_FILES["file"]["name"];
+    $sanitized_filename = htmlspecialchars(basename($_FILES["file"]["name"]), ENT_QUOTES, 'UTF-8');
+
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+        echo "File uploaded to /uploads/" . $sanitized_filename;
+    } else {
+        echo "There was an error uploading your file.";
+    }
 }
 ?>
+</body>
+</html>
+
 </body>
 </html>
